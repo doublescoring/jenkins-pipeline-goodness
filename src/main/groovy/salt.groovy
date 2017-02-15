85e7aa61c4bc98a381b65a6349e0f6ed6bf71a21
Alexey Aksenov <aaksenov@doublescoring.com>
2017-02-15 10:51:49 +0300

    Rewrite salt helper with regard of Jenkins CPS

--- src/main/groovy/salt.groovy
+++ src/main/groovy/salt.groovy
@@ -30,16 +30,19 @@ void writeConfig(String file, Map map) {
     writeFile encoding: 'UTF-8', file: file, text: generateConfig(map)
 }
 
-String getArrayBlock(List value, int level) {
+String getArrayBlock(List list, int level) {
     def output = ""
-    for (i = 0; i < value.size(); i++) {
-        if (Map.isAssignableFrom(value[i].getClass())) {
+    def iValue = 0
+    while (iValue < list.size()) {
+        def value = list[iValue]
+        if (Map.isAssignableFrom(value.getClass())) {
             output = output + "  " * (level + 1) + "-\n" + getMapBlock(value[i], level + 2)
-        } else if ([Collection, Object[]].any { it.isAssignableFrom(value[i].getClass()) }) {
+        } else if (Collection.isAssignableFrom(value.getClass()) || Object[].isAssignableFrom(value.getClass())) {
             output = output + "  " * level + "-\n" + getArrayBlock(value, level + 1)
         } else {
-            output = output + "  " * (level + 1) + "- " + value[i] + "\n"
+            output = output + "  " * (level + 1) + "- '$value'\n"
         }
+        iValue++
     }
     return output
 }
@@ -47,19 +50,19 @@ String getArrayBlock(List value, int level) {
 String getMapBlock(Map map, int level) {
     def output = ""
     def keys = map.keySet()
-    def keysSize = keys.size()
-    for (i = 0; i < keysSize; i++) {
-        def saveI = i
-        def value = map[keys[i]]
+    def iKey = 0
+    while (iKey < keys.size()) {
+        def key = keys[iKey]
+        def value = map[key]
         if (Map.isAssignableFrom(value.getClass())) {
-            output = output + "  " * level + keys[i] + ":\n" + getMapBlock(value, level + 1)
-        } else if ([Collection, Object[]].any { it.isAssignableFrom(value.getClass()) }) {
-            output = output + "  " * level + keys[i] + ":\n" + getArrayBlock(value, level + 1)
+            output = output + "  " * level + "'$key':\n" + getMapBlock(value, level + 1)
+        } else if (Collection.isAssignableFrom(value.getClass()) || Object[].isAssignableFrom(value.getClass())) {
+            output = output + "  " * level + "'$key':\n" + getArrayBlock(value, level + 1)
         } else {
-            output = output + "  " * level + keys[i] + ":" + value
+            output = output + "  " * level + "'$key':" + value
         }
-        i = saveI
         output = output + "\n"
+        iKey++
     }
     return output
 }
