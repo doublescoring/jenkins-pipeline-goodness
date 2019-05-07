41b1554021e105e35cff3fec4f5e05a1ab2d47ea
Pavel Shuvalov <pshuvalov@doublescoring.com>
2019-05-07 17:44:13 +0300

    Fix for invocation

--- src/test/groovy/airflowTest.groovy
+++ src/test/groovy/airflowTest.groovy
@@ -16,16 +16,33 @@
  *  limitations under the License.
  */
 
-import static org.junit.Assert.*;
+
 
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 
+import static org.junit.Assert.assertEquals
+
 class airflowTest {
+    GroovyShell shell
+    Binding binding
+    Script script
+    String scriptText = this.class.getResourceAsStream('testHeader.groovy').text + new File("src/main/groovy/airflow.groovy").text
 
     @Before
     void setUp() {
+        binding = new Binding()
+        binding.setVariable("fileExists", this.&fileExists)
+        binding.setVariable("readFile", this.&readFile)
+        binding.setVariable("sh", this.&sh)
+        binding.setVariable("timeout", this.&timeout)
+        binding.setVariable("waitUntil", this.&waitUntil)
+        shell = new GroovyShell(binding)
+        script = shell.parse(scriptText)
+        // fix Jenkins sleep
+        script.metaClass.static.sleep = { long ms -> Thread.sleep(ms * 1000) }
+        binding.setVariable("script", script)
     }
 
     @After
@@ -34,8 +51,9 @@ class airflowTest {
 
     @Test
     void testAirflowMessageParsing() {
-        def parsed = airflow.parseExecutionDate("Created <DagRun test_dag @ 2019-05-07 14:13:49+00:00: " +
-                "jenkins-#36_pshuvalov, externally triggered: True>", "test_dag")
+        def message = 'Created <DagRun test_dag @ 2019-05-07 14:13:49+00:00: jenkins-#36_pshuvalov, externally triggered: True>'
+        def testDag = 'test_dag'
+        def parsed = shell.evaluate('script.parseExecutionDate("' + message + '", "' + testDag + '")')
         assertEquals("2019-05-07T14:13:49", parsed)
     }
 }
