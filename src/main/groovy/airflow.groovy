e3b38d3bface166c3c9df88328e39739a9560240
Aleksandr <aiurkin@whoisblogger.com>
2021-03-16 12:36:52 +0500

    contentType fix

--- src/main/groovy/airflow.groovy
+++ src/main/groovy/airflow.groovy
@@ -34,7 +34,7 @@ String trigger(String airflowUrl, String dagName, Map dagParams = [], String run
     def body = new JsonBuilder(runDagRequest).toString()
     echo("Sending request to airflow ${body}")
     def airflowApiUrl = "${airflowUrl}/api/v1/dags/${dagName}/dagRuns"
-    def response = httpRequest url: airflowApiUrl, requestBody: body, httpMode: 'POST'
+    def response = httpRequest url: airflowApiUrl, requestBody: body, httpMode: 'POST', contentType: 'APPLICATION_JSON'
     echo("Response from Airflow: ${response.content}")
     assert(response.status == 200)
     def responseMessage = new JsonSlurperClassic().parseText(response.content)['message']
